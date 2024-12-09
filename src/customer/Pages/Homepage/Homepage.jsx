abe9908f295cd22b382ca1efab38ebbb1253ff85
Ananykumar1 <anany.21scs1011151@galgotiasuniversity.edu.in>
2024-12-10 00:26:29 +0530

    fix-dependencies

---
+++ src/customer/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import MainCarousel from '../../component/HomeCarousel/MainCarousel';
+
+const Homepage =()=>{
+    return(
+        <div>
+            <MainCarousel/>
+            <div>
+                other section
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
